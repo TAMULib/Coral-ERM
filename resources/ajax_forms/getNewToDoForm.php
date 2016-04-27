83b3e74966a8057974e4688f83cc42f539d5a1ef
jsavell <jsavell@library.tamu.edu>
2016-04-27 10:30:30 -0500

    use standard search for quick issn popup

--- resources/ajax_forms/getNewToDoForm.php
+++ resources/ajax_forms/getNewToDoForm.php
@@ -1,16 +1,17 @@
 <form id='quickToDoForm'>
 	<table class="thickboxTable" style="width:98%;background-image:url('images/title.gif');background-repeat:no-repeat;">
-		<tr>
+<!--		<tr>
 			<td><label>PO:</label></td>
 			<td>
 				<input type="text" name="search[po]" />
 				<span id='span_error_search_po' class='error smallDarkRedText'></span>
 			</td>
 		</tr>
+-->
 		<tr>
 			<td><label>ISSN:</label></td>
 			<td>
-				<input type="text" name="search[issn]" />
+				<input type="text" name="search[resourceISBNOrISSN]" id="toDoISBNOrISSN"/>
 				<span id='span_error_search_issn' class='error smallDarkRedText'></span>
 			</td>
 		</tr>
