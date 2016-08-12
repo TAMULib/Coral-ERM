593b40444beb30772c8fcbaf024ae1534498f523
jsavell <jsavell@library.tamu.edu>
2016-08-12 15:15:04 -0500

    make organizationID column nullable

--- resources/install/protected/resource_todo.sql
+++ resources/install/protected/resource_todo.sql
@@ -3,4 +3,4 @@ ALTER TABLE `ResourceStep` ADD `reviewDate` DATE NULL DEFAULT NULL;
 ALTER TABLE `ResourcePayment` ADD `purchaseOrder` VARCHAR( 24 ) NULL ;
 ALTER TABLE `ResourcePayment` ADD `systemID` VARCHAR( 24 ) NULL ;
 ALTER TABLE `ResourcePayment` ADD `vendorCode` VARCHAR( 48 ) NULL ;
-ALTER TABLE `Fund` ADD `organizationID` INT NOT NULL , ADD INDEX ( `organizationID` ) ;
+ALTER TABLE `Fund` ADD `organizationID` INT NULL , ADD INDEX ( `organizationID` ) ;
